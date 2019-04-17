9f75da243ddb69fad8c3fef44b7f2b490ffc658a
Yona168 <yginsburg26@gmail.com>
2019-04-17 00:01:10 -0400

    Game

--- battlesim/entity.py
+++ battlesim/entity.py
@@ -2,7 +2,7 @@ from inventory import Inventory
 from element import EntityType
 from random import uniform
 class Entity:
-    def __init__(self, health, inventory, speed, type=EntityType.NEUTRAL):
+    def __init__(self, name, health, inventory, speed, type=EntityType.NEUTRAL):
         self.health=health
         self.inventory=inventory
         self.speed=speed
@@ -16,8 +16,11 @@ class Entity:
 
     def damage(self, other_entity):
         __basic_dmg=((self._total_damage()-other_entity._armor_protection()))*self.type.multiplier(other_entity.type)
-        dmg_amt=abs(round(uniform(__basic_dmg-3, __basic_dmg+3), 2))
-        __weapon=self.inventory.current_weapon().damage
-
+        __offset=__basic_dmg*(.2*__basic_dmg)
+        dmg_amt=round(min(uniform(__basic_dmg-__offset, __basic_dmg+__offset), 0), 2)
         other_entity.health-=dmg_amt
+        self.inventory.damage_current_weapon()
         return dmg_amt
+
+    def is_dead(self):
+        return self.health==0
--- battlesim/game.py
+++ battlesim/game.py
@@ -19,3 +19,32 @@ class Turn:
             return (first, second)
 
         (faster, slower)=_resolve_speed()
+        faster_dmg=faster.damage(slower)
+        slower_dmg=0
+        if !slower.is_dead():
+            slower_dmg=slower.damage(faster)
+        self.faster_entity=faster
+        self.slower_entity=slower
+        self.faster_dmg=faster_dmg
+        self.slower_dmg=slower_dmg
+
+class Game:
+    def __init__(self,entity_one, entity_two, output_func):
+        self.entity_one=entity_one
+        self.entity_two=entity_two
+        self.output_func=output_func
+        self.turns=list()
+
+    def progress(self):
+        turn=Turn(self.entity_one, self.entity_two)
+        turn.resolve()
+        self.output_func(">>{0} hit {1} for {2} health!".format(turn.faster_entity.name, turn.slower_entity.name, turn.faster_dmg))
+        if turn.slower_entity.is_dead():
+            self.output_func(">>{0} died!".format(turn.slower_entity))
+        else:
+            self.output_func(">>{0} hit {1} for {2} health!".format(turn.slower_entity.name, turn.faster_entity.name, turn.slower_dmg))
+            end()
+        turns.append(turn)
+
+    def end(self):
+        self.output_func("Game has ended!")
